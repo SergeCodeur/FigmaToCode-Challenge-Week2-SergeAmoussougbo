fee1f861b09b9fab7fb32c34c923e09b08bbf7b4
SergeCodeur <amoussougboserge@gmail.com>
2024-02-02 18:38:57 +0100

    feat: ajout des sections BestSellers et Newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Banner from "@/components/sections/Banner";
+import BestSellers from "@/components/sections/BestSellers";
 import Header from "@/components/sections/Header";
 import HotCategory from "@/components/sections/HotCategory";
+import Newsletter from "@/components/sections/Newsletter";
 import Products from "@/components/sections/Products";
 import Zara from "@/components/sections/Zara";
 
@@ -12,6 +14,8 @@ export default function Home() {
       <HotCategory />
       <Products />
       <Zara />
+      <BestSellers />
+      <Newsletter />
     </main>
   );
 }

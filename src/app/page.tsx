a199789d686798205bb91412a39fee613efa853a
SergeCodeur <amoussougboserge@gmail.com>
2024-02-02 14:33:09 +0100

    feat: ajout de la section zara et suppression des commentaires provisoires

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,16 @@ import Banner from "@/components/sections/Banner";
 import Header from "@/components/sections/Header";
 import HotCategory from "@/components/sections/HotCategory";
 import Products from "@/components/sections/Products";
+import Zara from "@/components/sections/Zara";
 
 export default function Home() {
   return (
-    // <div className="h-dvh flex justify-center items-center">
-    //   <Button variant="small">
-    //     <FaFilter />
-    //     <span>Filter</span>
-    //   </Button>
-    //
-    //   <Button variant="line">
-    //     <FaRegPaperPlane />
-    //     <span>Submit</span>
-    //   </Button>
-    //   <Button variant="simple">
-    //     <FaBagShopping />
-    //     <span>Shop Now</span>
-    //   </Button>
-    //   <Ribbon variant="sale">Sale</Ribbon>
-    //   <Input type="text" placeholder="Email address..." />
-    //   <ItemTab variant="active">item</ItemTab>
-    //   <ItemMenu variant="larg">item</ItemMenu>
-    // </div>
     <main>
       <Header />
       <Banner />
       <HotCategory />
       <Products />
+      <Zara />
     </main>
   );
 }

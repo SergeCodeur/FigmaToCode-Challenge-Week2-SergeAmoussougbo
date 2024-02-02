f3bd6714d101f2d77ff7696513930657d7eac568
SergeCodeur <amoussougboserge@gmail.com>
2024-02-02 18:41:59 +0100

    refactor: ajout du style transparent

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex py-[13px] px-[15px] border-b-2 border-b-dark text-sm font-semibold ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/50 focus-visible:outline-none focus-visible:ring-O focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "bg-transparent flex py-[13px] px-[15px] border-b-2 border-b-dark text-sm font-semibold ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/50 focus-visible:border-b-primary focus-visible:outline-none focus-visible:ring-O focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
